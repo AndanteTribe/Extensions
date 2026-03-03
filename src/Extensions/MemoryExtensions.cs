e28611a68f24bc008537032d190bdd1158e6bd1f
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2026-03-04 02:44:17 +0900

    feat: initial up

---
+++ src/Extensions/MemoryExtensions.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="Memory{T}"/> and <see cref="ReadOnlyMemory{T}"/>.
+/// </summary>
+public static class MemoryExtensions
+{
+    /// <summary>
+    /// Enables foreach iteration over a <see cref="Memory{T}"/>.
+    /// </summary>
+    /// <remarks>
+    /// This method works the same way as <see cref="GetEnumerator{T}(in ReadOnlyMemory{T})"/>.
+    /// </remarks>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// using System;
+    ///
+    /// public class Example
+    /// {
+    ///     public static void Main()
+    ///     {
+    ///         const string text = "Hello, World!";
+    ///         Memory<char> textMemory = text.AsMemory().Slice(7, 5); // 'W', 'o', 'r', 'l', 'd'
+    ///
+    ///         foreach (var c in textMemory)
+    ///         {
+    ///             Console.WriteLine(c);
+    ///         }
+    ///         // Output:
+    ///         // W
+    ///         // o
+    ///         // r
+    ///         // l
+    ///         // d
+    ///     }
+    /// }
+    /// ]]>
+    /// </code>
+    /// </example>
+    /// <param name="memory">The target <see cref="Memory{T}"/>.</param>
+    /// <typeparam name="T">The element type.</typeparam>
+    /// <returns>An <see cref="MemoryExtensions.Enumerator{T}"/> that enumerates the elements.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Enumerator<T> GetEnumerator<T>(in this Memory<T> memory) => new(memory);
+
+    /// <summary>
+    /// Enables foreach iteration over a <see cref="ReadOnlyMemory{T}"/>.
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// using System;
+    ///
+    /// public class Example
+    /// {
+    ///     public static void Main()
+    ///     {
+    ///         const string text = "Hello, World!";
+    ///         ReadOnlyMemory<char> textMemory = text.AsMemory().Slice(7, 5); // 'W', 'o', 'r', 'l', 'd'
+    ///
+    ///         foreach (var c in textMemory)
+    ///         {
+    ///             Console.WriteLine(c);
+    ///         }
+    ///         // Output:
+    ///         // W
+    ///         // o
+    ///         // r
+    ///         // l
+    ///         // d
+    ///     }
+    /// }
+    /// ]]>
+    /// </code>
+    /// </example>
+    /// <param name="memory">The target <see cref="ReadOnlyMemory{T}"/>.</param>
+    /// <typeparam name="T">The element type.</typeparam>
+    /// <returns>An <see cref="MemoryExtensions.Enumerator{T}"/> that enumerates the elements.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Enumerator<T> GetEnumerator<T>(in this ReadOnlyMemory<T> memory) => new(memory);
+
+    /// <summary>
+    /// Enumerator for foreach iteration over <see cref="ReadOnlyMemory{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The element type.</typeparam>
+    public struct Enumerator<T>
+    {
+        private readonly ReadOnlyMemory<T> _memory;
+        private int _index;
+
+        /// <see cref="System.Collections.Generic.IEnumerator{T}.Current"/>
+        public readonly T Current => _memory.Span[_index];
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Enumerator{T}"/> struct.
+        /// </summary>
+        /// <param name="memory">The <see cref="ReadOnlyMemory{T}"/> to enumerate.</param>
+        internal Enumerator(in ReadOnlyMemory<T> memory)
+        {
+            _memory = memory;
+            _index = -1;
+        }
+
+        /// <see cref="System.Collections.IEnumerator.MoveNext"/>
+        public bool MoveNext() => _index < _memory.Length && ++_index < _memory.Length;
+    }
+}
---
+++ src/Extensions/ValueTupleExtensions.cs
@@ -0,0 +1,90 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="ValueTuple"/>.
+/// </summary>
+public static class ValueTupleExtensions
+{
+    /// <summary>
+    /// Converts a 2-element ValueTuple to a <see cref="ReadOnlySpan{T}"/>.
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// public class Example
+    /// {
+    ///     public static void Main()
+    ///     {
+    ///         // Iterate through the span
+    ///         foreach (var item in ("Hello", "World").AsSpan())
+    ///         {
+    ///             Console.WriteLine(item);
+    ///         }
+    ///         // Output:
+    ///         // Hello
+    ///         // World
+    ///     }
+    /// }
+    /// ]]>
+    /// </code>
+    /// </example>
+    /// <typeparam name="T">The type of elements in the tuple.</typeparam>
+    /// <param name="tuple">The 2-element ValueTuple to convert.</param>
+    /// <returns>A <see cref="ReadOnlySpan{T}"/> with 2 elements.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReadOnlySpan<T> AsSpan<T>(this (T, T) tuple) =>
+        MemoryMarshal.CreateReadOnlySpan(ref tuple.Item1, 2);
+
+    /// <summary>
+    /// Converts a 3-element ValueTuple to a <see cref="ReadOnlySpan{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the tuple.</typeparam>
+    /// <param name="tuple">The 3-element ValueTuple to convert.</param>
+    /// <returns>A <see cref="ReadOnlySpan{T}"/> with 3 elements.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReadOnlySpan<T> AsSpan<T>(this (T, T, T) tuple) =>
+        MemoryMarshal.CreateReadOnlySpan(ref tuple.Item1, 3);
+
+    /// <summary>
+    /// Converts a 4-element ValueTuple to a <see cref="ReadOnlySpan{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the tuple.</typeparam>
+    /// <param name="tuple">The 4-element ValueTuple to convert.</param>
+    /// <returns>A <see cref="ReadOnlySpan{T}"/> with 4 elements.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReadOnlySpan<T> AsSpan<T>(this (T, T, T, T) tuple) =>
+        MemoryMarshal.CreateReadOnlySpan(ref tuple.Item1, 4);
+
+    /// <summary>
+    /// Converts a 5-element ValueTuple to a <see cref="ReadOnlySpan{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the tuple.</typeparam>
+    /// <param name="tuple">The 5-element ValueTuple to convert.</param>
+    /// <returns>A <see cref="ReadOnlySpan{T}"/> with 5 elements.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReadOnlySpan<T> AsSpan<T>(this (T, T, T, T, T) tuple) =>
+        MemoryMarshal.CreateReadOnlySpan(ref tuple.Item1, 5);
+
+    /// <summary>
+    /// Converts a 6-element ValueTuple to a <see cref="ReadOnlySpan{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the tuple.</typeparam>
+    /// <param name="tuple">The 6-element ValueTuple to convert.</param>
+    /// <returns>A <see cref="ReadOnlySpan{T}"/> with 6 elements.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReadOnlySpan<T> AsSpan<T>(this (T, T, T, T, T, T) tuple) =>
+        MemoryMarshal.CreateReadOnlySpan(ref tuple.Item1, 6);
+
+    /// <summary>
+    /// Converts a 7-element ValueTuple to a <see cref="ReadOnlySpan{T}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the tuple.</typeparam>
+    /// <param name="tuple">The 7-element ValueTuple to convert.</param>
+    /// <returns>A <see cref="ReadOnlySpan{T}"/> with 7 elements.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ReadOnlySpan<T> AsSpan<T>(this (T, T, T, T, T, T, T) tuple) =>
+        MemoryMarshal.CreateReadOnlySpan(ref tuple.Item1, 7);
+}
